aa834879ec02c8b9389bdb2096e9a0681f7b5914
ArmaCoder95 <amatienzo@plusap.pe>
2022-09-25 19:06:23 +0000

    formateador para MicroChart en el controlador

--- webapp/controller/materialOverView.controller.js
+++ webapp/controller/materialOverView.controller.js
@@ -1,13 +1,15 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "ui5/samplecoreelements/util/formatter"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, formatter) {
         "use strict";
 
         return Controller.extend("ui5.samplecoreelements.controller.materialOverView", {
+            formatter: formatter,
             onInit: function () {
 
             }
